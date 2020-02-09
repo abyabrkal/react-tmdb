b6a55da5d3f5443a4f6732351c2d0a5a1747aa2a
Aby Abraham <abyabrkal@gmail.com>
2020-02-09 13:36:46 +0400

    Header styling completed

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,5 +1,34 @@
 import React from 'react'
 
-const Header = () => <div>Header</div>
+import './header.css'
+import MoveeLogo from '../../images/brand.png';
+import TMDBlogo from '../../images/tmdb_logo.svg';
+
+const Header = () => (
+    <div className="box-border px-0 py-5">
+        <div className="max-w-screen-xl min-h-48 px-5 py-0 mx-0 my-auto box-border sm:m-w-screen-xl sm:min-h-0">
+            <img className="inline-block mx-0 w-32 sm:mt-2 sm:w-56" src={MoveeLogo} alt="movie-logo" />
+            <img className="w-20 mt-6 sm:inline-block sm:mt-16 sm:w-24 float-right inline-block" src={TMDBlogo} alt="tmdb-logo" />
+        </div>
+    </div>
+)
+
+// const Header = () => (
+//     <div className="box-border px-0 py-5">
+//         <div className="max-w-screen-xl min-h-48 px-5 py-0 mx-0 my-auto box-border sm:m-w-screen-xl sm:min-h-0">
+//             <img className="w-32 sm:mt-2 sm:w-64" src={MoveeLogo} alt="movie-logo" />
+//             <img className="float-right inline-block w-20 mt-6 sm:inline-block sm:mt-3 sm:w-32" src={TMDBlogo} alt="tmdb-logo" />
+//         </div>
+//     </div>
+// )
+
+// const Header = () => (
+//     <div className="">
+//         <div className="header-content">
+//             <img className="MoveeLogo" src={MoveeLogo} alt="movie-logo" />
+//             <img className="TMDBLogo" src={TMDBlogo} alt="tmdb-logo" />
+//         </div>
+//     </div>
+// )
 
 export default Header;
