10ef619e8058b8ec0f38e6e49dd1bdf6b66a3ff0
Aby Abraham <abyabrkal@gmail.com>
2020-02-09 12:01:44 +0400

    Fetching movies data for Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Grid from './elements/Grid'
 import Header from './elements/Header'
@@ -8,7 +8,40 @@ import MovieThumb from './elements/MovieThumb'
 import SearchBar from './elements/SearchBar'
 import Spinner from './elements/Spinner'
 
+import { API_URL, API_KEY } from '../config';
+
 const Home = () => {
+    const [state, setState] = useState({ movies: []});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const fetchMovies = async endpoint => {
+        setError(false);
+        setLoading(true);
+
+        try {
+            const result = await(await fetch(endpoint)).json();
+            console.log(result)
+            setState(prev => ({
+                ...prev,
+                movies: [...result.results],
+                heroImage: prev.heroImage || result.results[0],
+                currentPage: result.page,
+                totalPages: result.total_pages
+            }))
+            
+        } catch (error) {
+            setError(true);
+            console.log(error)
+        }
+        
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+    }, [])
+
     return (
         <>
             <HeroImage />
