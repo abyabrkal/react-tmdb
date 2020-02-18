13e7b1c653a2b14fada0e867de25a8e4ac26b4fb
Aby Abraham <abyabrkal@gmail.com>
2020-02-18 08:44:49 +0400

    NotFound image

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import Oops from '../images/404.png'
+
 const NotFound = () => {
+    const bgNFImage = () => ({ 
+        backgroundImage: `${Oops}`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+    })
+
     return (
-        <div>
-            404: Not Found
+        <div className="w-full h-100p" style={bgNFImage()}>          
         </div>
     )
 }
